bbeb527247a8b737a162149ebc21a1f72d75f33a
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-07-02 22:31:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/SOCIAL-MECHANIC-TEAM-NRD/INLINE-CALCULATOR.git /Calculator-Bot
+git clone https://github.com/SOCIAL-MECHANIC-1997/INLINE-CALCULATOR.git /Calculator-Bot
 cd /Calculator-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
